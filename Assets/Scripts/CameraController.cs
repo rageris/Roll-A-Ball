8aee243d755345988a5828f101dd6f60d49ebe56
Joel Arroyo <joelarroyo1203@gmail.com>
2019-01-27 18:40:17 -0500

    Finished Roll A Ball

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+	public float smooth;
+	private Vector3 offset;
+	private Vector3 target;
+
+	void Start ()
+    {
+		offset = transform.position - player.transform.position;
+	}
+	
+	void FixedUpdate ()
+    {
+		target = player.transform.position + offset;
+	}
+	
+	void LateUpdate ()
+    {
+		//target.position = player.transform.position + offset; a is where i am at b is where i want to be
+		transform.position = Vector3.Lerp(transform.position,  target, smooth*Time.deltaTime);
+	}
+
+}
---
+++ Assets/Scripts/ManagerOfScenes.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ManagerOfScenes : MonoBehaviour {
+
+	public void loadthing()
+	{
+		SceneManager.LoadScene ("Roll a Ball");
+	}
+
+	public void loadmenu()
+	{
+		SceneManager.LoadScene ("Main_menu");
+	}
+}
